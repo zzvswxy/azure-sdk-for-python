2f2801aaab3deeeb6240138ba9172cec00c31ced
Scott Beddall <scbedd@microsoft.com>
2022-01-14 17:14:05 -0800

    apply black

--- scripts/devops_tasks/use_pypy_version.py
+++ scripts/devops_tasks/use_pypy_version.py
@@ -1,15 +1,7 @@
 import platform
-import json
 import argparse
-import urllib
-import urllib.request
-from subprocess import check_call, CalledProcessError
 import sys
 import os
-import zipfile
-import tarfile
-import time
-
 from packaging.version import Version
 from packaging.version import InvalidVersion
 
@@ -19,33 +11,55 @@ CURRENT_UBUNTU_VERSION = "20.04"  # full title is ubuntu-20.04
 MAX_PRECACHED_VERSION = "3.10.0"
 HOSTEDTOOLCACHE = os.getenv("AGENT_TOOLSDIRECTORY")
 
+
 def walk_directory_for_pattern(spec):
     target_directory = os.path.normpath(HOSTEDTOOLCACHE)
-    
+    pypy_tool_cache = os.path.join(target_directory, "PyPy")
+
     print("Searching for {} in hosted tool cache {}".format(spec, target_directory))
     located_folders = []
 
+    discovered_tool_folders = os.listdir(pypy_tool_cache)
+
     # walk the folders, filter to the patterns established
-    for folder, subfolders, files in os.walk(target_directory):
-        for file in files:
-            file_path = os.path.join(folder, file)
-            print(file_path)
+    for folder in discovered_tool_folders:
+        path, foldername = os.path.split(folder)
+        tool_version = Version(folder)
+
+        if tool_version.major == spec.major and tool_version.minor == spec.minor:
+            found_tool_location = os.path.join(pypy_tool_cache, folder)
+
+            # logic for this location is cribbed directly from existing UsePythonVersion type script task
+            # https://github.com/microsoft/azure-pipelines-tasks/blob/master/Tasks/UsePythonVersionV0/usepythonversion.ts#L32
+            # Path should first be prepended by the install location, then by the executables directory.
+            # The executable directory is <installDir>/bin on Ubuntu/Mac, <installDir>/Scripts on Windows
+            # Additionally on windows, there are only x86 versions of the tool. on unix/mac there only exist x64 versions of the tool.
+            if platform.system() == "Windows":
+                install_path = os.path.join(found_tool_location, "x86")
+                tool_path = os.path.join(install_path, "Scripts")
+
+                located_folders.extend([install_path, tool_path])
+            else:
+                install_path = os.path.join(found_tool_location, "x64")
+                tool_path = os.path.join(install_path, "bin")
+
+                located_folders.extend([install_path, tool_path])
 
     return located_folders
 
+
 def find_pypy_version(spec):
-    pypy_spec = "pypy" + str(spec)
     discovered_locations = walk_directory_for_pattern(spec)
 
     if not discovered_locations:
         print(
             "Unable to locate a valid executable folder for {}. Examined folder {}".format(
-                pypy_spec, HOSTEDTOOLCACHE
+                str(spec), HOSTEDTOOLCACHE
             )
         )
         exit(1)
 
-    return []
+    return discovered_locations
 
 
 if __name__ == "__main__":
@@ -59,7 +73,7 @@ if __name__ == "__main__":
         nargs="?",
         help=("The version specifier passed in to the UsePythonVersion extended task."),
     )
-    
+
     args = parser.parse_args()
     max_precached_version = Version(MAX_PRECACHED_VERSION)
     try:
@@ -72,7 +86,7 @@ if __name__ == "__main__":
 
     print(
         "Path should be prepended with discovered location{} [{}]".format(
-            ("s" if discovered_installer_location >= 2 else ""),
+            ("s" if len(discovered_installer_location) >= 2 else ""),
             ", ".join(discovered_installer_location),
         )
     )
