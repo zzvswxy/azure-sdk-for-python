5987f312a789c57e886ac63217fe28b3d8b8ac6c
azure-sdk <no-reply@azure-sdk.com>
2022-06-26 16:52:45 +0000

    [auto]create by sdk generation

--- sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/__init__.py
+++ sdk/agrifood/azure-mgmt-agrifood/azure/mgmt/agrifood/__init__.py
@@ -10,10 +10,14 @@ from ._azure_agri_food_rp_service import AzureAgriFoodRPService
 from ._version import VERSION
 
 __version__ = VERSION
-__all__ = ['AzureAgriFoodRPService']
 
 try:
-    from ._patch import patch_sdk  # type: ignore
-    patch_sdk()
+    from ._patch import __all__ as _patch_all
+    from ._patch import *  # type: ignore # pylint: disable=unused-wildcard-import
 except ImportError:
-    pass
+    _patch_all = []
+from ._patch import patch_sdk as _patch_sdk
+__all__ = ['AzureAgriFoodRPService']
+__all__.extend([p for p in _patch_all if p not in __all__])
+
+_patch_sdk()
